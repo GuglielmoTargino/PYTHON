aa3f068b5b0f103cde6ad2b959cb178ca26f46fd
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-07-30 20:12:38 -0300

    cap4_listas
    Aulas com lista aninhadas

---
+++ PY-DSA/aula4/index_string.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 19:50:23 2024
+
+@author: Guglielmo H T
+Vesion: v0
+"""
+
+s='guglielmo';
+print(s);
+g=s.upper() # necessario uma variavel para receber a conversão
+print(g)
+g=s.capitalize();
+print(g);
+g=s.count('g');
+print(g);
+g=s.endswith('m');
+print(g);
+
+print("guga"=="Guga");
+
+ 
+
---
+++ PY-DSA/aula4/listas_aninhadas.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 19:50:23 2024
+
+@author: Guglielmo H T
+Vesion: v0
+"""
+
+lista_1=["guga,dara,nia"]; # lista sempre usa colchetes []
+print(lista_1);
+
+lista_2=["todi","joe","guga"];
+print(lista_2);
+print(lista_2[1]);
+print(lista_2[2]);
+lista_2[2]='nia';
+print(lista_2[2]);
+#print(lista_1[1]); lista de uma string apenas. sem indice
+lista_3=[2.1,6,"nia"];
+print(lista_3);
+
+res=type(lista_3[0]);
+print(res);
+
