f118190e1a1ea1bd796a9fac822d299cfc781ab3
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-08-16 16:23:02 -0300

    cap5_loop_dic
    Loop com dicionarios

---
+++ PY-DSA/P1_TRI5/41-Cap05/Lab2/loop_dict.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 17:42:05 2024
+
+@author: Guglielmo H T
+"""
+
+dict1={'k1':'nia','k2':'dara','k3':'joe'}
+
+for g in dict1.items():
+    print(g)
+
+
+
+
+   
+    
