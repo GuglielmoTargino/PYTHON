5fe32caac3db75b3e9ae954a504a058f8170207a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-08-17 00:04:32 -0300

    alfa_loop_while
    Continua o exercício while

--- PY-DSA/P1_TRI5/41-Cap05/Lab2/loop_while_lista2.py
+++ PY-DSA/P1_TRI5/41-Cap05/Lab2/loop_while_lista2.py
@@ -10,12 +10,20 @@ Versão: v0
         
     
 
-a=11
+a=10
 i=1
+x=0
+y=0
+cont=0
+
+
+
 while i<=a:
-    if a//i==1 and a%i==0:
-        print (' o numero  primo é',a)
+    if a//i
+    
+        print(i)
+      
     i+=1
         
-print('fim loop')
+print('fim loop',cont)
 
