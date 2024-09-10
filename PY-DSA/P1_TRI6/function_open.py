dc615e344294b34d28a9e58d11a87e3d1ff15f7c
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-09-10 17:41:47 -0300

    conti_function_open
    Continuação da função open

---
+++ PY-DSA/P1_TRI6/function_open.py
@@ -0,0 +1,8 @@
+#execício com linguagem python do curso data Science Academy
+
+
+arqv=open("C:/Users\Guglielmo H T/Desktop/testepy.txt","r")
+ler=arqv.read()
+leitura=ler.split(" ")
+
+print(leitura[1])
