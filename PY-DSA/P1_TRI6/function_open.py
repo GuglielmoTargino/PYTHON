3a06d3ee616eaafee96821b3e048f3bce5bc07ef
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-09-11 14:21:31 -0300

    exer_fopen
    Exercício com function open.

--- PY-DSA/P1_TRI6/function_open.py
+++ PY-DSA/P1_TRI6/function_open.py
@@ -1,8 +1,5 @@
 #execício com linguagem python do curso data Science Academy
 
 
-arqv=open("C:/Users\Guglielmo H T/Desktop/testepy.txt","r")
-ler=arqv.read()
-leitura=ler.split(" ")
-
-print(leitura[1])
+arqv=open("C:/Users/Guglielmo H T/Desktop/testepy.txt","w")
+arqv.write("olá future! i am here!")
