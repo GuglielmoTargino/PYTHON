0d31a9909f0b53baaa3c68b50aff8c8c81fafccf
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-10-03 16:08:40 -0300

    ini_with_func
    Iniciado o exercício com função with.

---
+++ PY-DSA/P1_TRI6/with_func.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+#Aula DSA tr6.
+#Aluno Guglielmo Targino.
+#versão v0
+#Data 03out24
+"""
+with open('C:/Users/Guglielmo H T/Desktop/logue.txt','r') as arquivo:
+    conteudo=arquivo.read()
+    print(conteudo)
