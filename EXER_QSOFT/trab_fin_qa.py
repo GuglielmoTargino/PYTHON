03cc33ffc4710bc4f8655c7343a499bedd2a286e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-10-18 17:40:06 -0300

    v0_trab_fin_qa
    Versão final do trabalho de qualidade de software.

--- EXER_QSOFT/trab_fin_qa.py
+++ EXER_QSOFT/trab_fin_qa.py
@@ -1,7 +1,7 @@
 #Trabalho final qld software.
 #Aluno Guglielmo Targino.
 #versão v1.
-#Data 17out24
+#Data 10nov24
 
 import unittest
 
@@ -16,7 +16,7 @@ class Pessoa:
     def calcImc(self):
         
         #verifica se algum valor de entrada é menor ou igual a zero.        
-        if (self.altura and self.peso):
+        if (self.altura):
             
             #fórmula para cálculo do imc
             return self.peso/(self.altura**2)
@@ -25,9 +25,9 @@ class Pessoa:
         
 
 #prepara variáveis para testes
-alt=0.62 #variavel para altura
-peso=595 #variavel para peso
-esperado=1547.87 #variavel para valor esperado
+alt=1.73 #variavel para altura
+peso=73.6 #variavel para peso
+esperado=24.59 #variavel para valor esperado
 
 # Criando um objeto da classe Pessoa
 pessoa1 = Pessoa(alt,peso)
@@ -35,15 +35,19 @@ pessoa1 = Pessoa(alt,peso)
 
 #testes unitários inicia aqui.
 class testeImc(unittest.TestCase):
-     
+    
     def test_imcIgual(self):
         #around arredondado para duas casas decimais
         #Aqui executa testes efetivos no método calcImc().
         self.assertEqual(round(pessoa1.calcImc(),2),esperado)
         
-        #aqui verifica o teste de valor inferior ou igual a zero foi digitado.
+        #aqui verifica o teste de valor igual a zero.
     def test_Falso(self):
-        self.assertTrue(round(pessoa1.calcImc()),"Por favor digite numeros positivos.")
+        self.assertTrue(round(pessoa1.calcImc()),"Favor digitar números maiores que 0.")
+     
+        #aqui verifica o teste de valor negativo.        
+    def test_Neg(self):
+         self.assertLess(round(pessoa1.calcImc()),0)
         
 
 if __name__ == '__main__':
