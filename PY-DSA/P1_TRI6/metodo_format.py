6aeb66b020ea47648cfd18639f86cd7882f07621
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-10-23 18:05:53 -0300

    metodo_format
    Metodo format().

---
+++ PY-DSA/P1_TRI6/metodo_format.py
@@ -0,0 +1,8 @@
+#execício com linguagem python do curso data Science Academy
+#Treino com a função reduce
+
+
+
+n=input('informe um numero')
+print(f'voce digitou {n}')
+print('E também {1},{1},{1}'.format(n,9,4))
