7eae8bfd4d2c91feddc3edbc1f5c2e5648bb305d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-10-30 17:33:18 -0300

    treino_regex
    Exercício com comando regex

--- PY-DSA/P1_TRI6/reg_expres.py
+++ PY-DSA/P1_TRI6/reg_expres.py
@@ -1,4 +1,5 @@
 #exercício com expressões regulares
+# https://docs.python.org/3.9/library/re.html
 #Aluno:GuglielmoTargino.
 #Dta:30out24
 #versão:v0.
@@ -7,5 +8,19 @@
 
 import re
 
-texto1='minha rota minha vida guglielmotargino@gmail.com'
+texto1='minha rota minha vidaaa! guglielmotargino@gmail.com'
+texto2 = "Contatos: pessoa1@example.com, suporte@dominio.org, e teste.email@empresa.com.br"
+
+ocorrencias = re.findall(r'a', texto1, re.IGNORECASE)
+print(f"A letra 'a' aparece {len(ocorrencias)} vezes.")
+
+
+
+emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', texto1)
+print("E-mails encontrados:", emails)
+
+palavras_com_exclamacao = re.findall(r'\b\w+!', texto1)
+print("Palavras com exclamação:",palavras_com_exclamacao)
+
+
 
