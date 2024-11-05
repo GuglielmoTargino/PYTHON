4793a7e36c19f1502b660520c3d7e6745b4e5886
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-11-05 18:34:10 -0300

    cont_teste
    Continua o teste com jogo a forca

--- PY-DSA/P1TRI7/jg1forca.py
+++ PY-DSA/P1TRI7/jg1forca.py
@@ -1,26 +1,16 @@
-#exercício com expressões regulares
+#jogo em python da forca 
 # https://docs.python.org/3.9/library/re.html
 #Aluno:GuglielmoTargino.
-#Dta:30out24
+#Dta:05nov24
 #versão:v0.
 
 
 
-import re
+from os import name, system
 
-texto1='minha rota minha vidaaa! guglielmotargino@gmail.com'
-texto2 = "Contatos: pessoa1@example.com, suporte@dominio.org, e teste.email@empresa.com.br"
-
-ocorrencias = re.findall(r'a', texto1, re.IGNORECASE)
-print(f"A letra 'a' aparece {len(ocorrencias)} vezes.")
-
-
-
-emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', texto1)
-print("E-mails encontrados:", emails)
-
-palavras_com_exclamacao = re.findall(r'\b\w+!', texto1)
-print("Palavras com exclamação:",palavras_com_exclamacao)
-
-
-
+if name == 'nt':
+    system("cls")  # Windows
+   
+else:
+    system("clear")  # Linux/Mac
+   
