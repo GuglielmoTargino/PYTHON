d57d4bdf75ca1305094edad92638f1e5873858a1
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-11-07 19:53:07 -0300

    jg1forca_v0
    Finalizada a logica do jogo da forca

--- PY-DSA/P1TRI7/jg1forca.py
+++ PY-DSA/P1TRI7/jg1forca.py
@@ -11,65 +11,67 @@ from os import name, system
 def limpa_tela():
     if name == 'nt':
         system("cls")  # Windows
-   
     else:
         system("clear")  # Linux/Mac
         
-
-palavra=['jaca','agua','uva','dara','nia']
-segredo=random.choice(palavra)
-print('Bem vindo ao jogo da forca')
-
-chance=len(segredo)
-revelacao=[]
-print(segredo)
-for x in segredo:
-    revelacao.append('_')
+def game_forca():
     
+    palavra=['jaca','agua','uva','dara','nia']
+    segredo=random.choice(palavra)
+    print('Bem vindo ao jogo da forca')
 
-print(chance)
-print(revelacao)
-
-cnt=0
-pt=0
-lb=0
-bt=0
-
-
-
-while cnt<chance:
-    lb=0
-    pt=0
-    jogador=input('Digite Uma letra por favor_')
+    chance=len(segredo)
+    revelacao=[]
+    print(segredo)
     for x in segredo:
-        if x==jogador:
-            revelacao[pt]=jogador
-            pt+=1
-        else:            
-            pt+=1
-   
-    cnt+=1
-    print(revelacao)  
-    print(f'Você tem {chance-cnt} chances')
-    
-    for y in segredo:
-        if segredo[lb]==revelacao[lb]:
-            
-            bt+=1
-            print('IGUAL')
-        else:
-            bt=0
-            print('NAO IGUAL')            
-        lb+=1
+        revelacao.append('_')
         
-        if bt==chance:
-            print('VOCE GANHOU!')
+
+    print(chance)
+    print(revelacao)
+
+    cnt=0
+  
+    while cnt<chance:
+        pt=0
+        lb=0
+        bt=0
+     
+        jogador=input('Digite Uma letra por favor_')
         
-    
+        for x in segredo:
+            if x==jogador:
+                revelacao[pt]=jogador
+                pt+=1
+            else:            
+                pt+=1   
+        cnt+=1
+        
+        print(revelacao)  
+        print(f'Você tem {chance-cnt} chances')
+        
+        for y in revelacao:
+         
+            if y =='_' and chance-cnt<1:
+                print('GAME OVER')
+                break
+        
+        for y in segredo:
+            if segredo[lb]==revelacao[lb]:            
+                bt+=1 #incrementa para testar '_'
+            else:
+                bt=0 #zera a contagem '_'            
+            lb+=1
             
-      
-    
-print('FIM')
+            if bt==chance: #se o numero de '_' for iguala chances jogo gnho
+                print('VOCE GANHOU!')
+                break           
+          
+        
+    print('FIM')
+
+if __name__ == '__main__':
+    game_forca()
 
     
     
