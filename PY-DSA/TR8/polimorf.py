e3c22ada92d25c021fb05fb4cbc3a5e3e05021b9
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2024-12-05 17:58:06 -0300

    xr_polimorf
    Exercício polimorfismo

---
+++ PY-DSA/TR8/polimorf.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 15:44:27 2024
+
+@author: Guglielmo H T
+"""
+
+class Animal():
+    def __init__(self,som,anda):
+        
+        self.som=som
+        self.anda=anda        
+        
+    def Soar(self):
+        pass
+
+class Gato(Animal):
+    
+    def Soar():
+        print("Gato mia")
+        
+class Cachorro(Animal):
+    
+    def Soar():
+        
+        print("Cachorro late")
+               
+               
+        
+
+lic=Cachorro("late", "pula")
+
+
+
+        
+        
+        
+        
+        
