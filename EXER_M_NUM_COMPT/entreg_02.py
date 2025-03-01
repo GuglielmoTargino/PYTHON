988f7939e0b17a325720fc70dbe871320226c898
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-03-01 10:48:05 -0300

    ok_atv_mnum_comp
    Finalizado o exercício de metodos numéricos.

--- EXER_M_NUM_COMPT/entreg_02.py
+++ EXER_M_NUM_COMPT/entreg_02.py
@@ -5,12 +5,21 @@ Created on Sat Mar  1 09:29:29 2025
 @author: Guglielmo H T
 """
 
-# Correlação de pearson entre o tempo de estudo e a nota do aluno
+# Correlação de pearson entre o tempo de exercício por dia e perda de peso do atleta.
+# Estudo teórico avaliando 10 atletas durante 30 dias.
 import numpy as np
 
-x = np.array([1, 2, 3, 4, 5,6]) # tempo de estudo
-y = np.array([2, 5, 7, 4, 9,9]) # nota do aluno
+hora = np.array([0.2, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]) # horas de exercício
+peso = np.array([0.1, 0.3, 0.3, 0.6, 0.8, 0.8,0.8,0.9,1]) #perda de peso em kg
 
-r = np.corrcoef(x, y)[0, 1]
+r = np.corrcoef(hora,peso)[0, 1]
 
-print("O valor do coeficiente e: ", r)
+print(f"O valor do coeficiente e:{r:.3f} ")
+
+import matplotlib.pyplot as plt
+
+plt.scatter(hora,peso)
+plt.xlabel("Tempo de exercício em horas por dia")
+plt.ylabel("Perda de peso em kg")
+plt.title("Correlação entre Tempo de exercício e perda de peso")
+plt.show()
