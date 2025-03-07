ffd7a0ba223cc6fdae5f819647a782120cc66823
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-03-07 18:06:50 -0300

    fim_tr9
    Finalizada a trilha nove

--- PY-DSA/TR9/test_media.py
+++ PY-DSA/TR9/test_media.py
@@ -6,7 +6,8 @@ Created on Fri Mar  7 15:43:00 2025
 """
 import numpy as gu
 
-arr5=gu.array([2,3,5,9])
+arr5=gu.array([2,3,5,9,1])
 
 resu=gu.mean(arr5)
-print(resu)
+resu2=gu.std(arr5)
+print(resu2)
